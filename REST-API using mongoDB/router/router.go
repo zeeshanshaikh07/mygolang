d504fde0e7c8b8cce76476b949da88ddcb19b923
Zeeshan Shaikh <zeeshanshaikh790.zs@gmail.com>
2022-03-30 14:26:47 +0530

    APIs using mongoDB

---
+++ REST-API using mongoDB/router/router.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/zeeshanshaikh07/mongoapi/controller"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	
+	router.HandleFunc("/api/movies", controller.GetMyAllMovies).Methods("GET")
+	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
+	router.HandleFunc("/api/movie/{id}", controller.MarkAsWatched).Methods("PUT")
+	router.HandleFunc("/api/movie/{id}", controller.DeleteAMovie).Methods("DELETE")
+	router.HandleFunc("/api/deleteallmovie", controller.DeleteAllMovies).Methods("DELETE")
+	return router
+}
